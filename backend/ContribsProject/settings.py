6a31f5cdddedfd9cbfb03194dbe5212557addc6d
benstearman <benstearman@users.noreply.github.com>
2026-02-09 10:05:28 -0800

    adding pagination

--- backend/ContribsProject/settings.py
+++ backend/ContribsProject/settings.py
@@ -138,3 +138,8 @@ CSRF_TRUSTED_ORIGINS = [
     for origin in os.environ.get("CSRF_TRUSTED_ORIGINS", "https://contribs.app").replace(',', ' ').split() 
     if origin.strip()
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
