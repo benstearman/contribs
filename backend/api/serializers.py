057b4c5318545e545d96641680ad972dbf637303
benstearman <benstearman@users.noreply.github.com>
2026-02-09 10:02:28 -0800

    changing to ModelSerializer temporarily

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,13 +2,13 @@ from .models import Contribution, Contributor
 from rest_framework import serializers
 
 
-class ContributionSerializer(serializers.HyperlinkedModelSerializer):
+class ContributionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contribution
         fields = ["contributor", "committee", "amount", "receipt_date"]
 
 
-class ContributorSerializer(serializers.HyperlinkedModelSerializer):
+class ContributorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contributor
         fields = ["zip_code", "full_name", "employer"]
