9ccbaf4d7b8446efb7ac6e0ae8027ff2cdd5afd2
benstearman <benstearman@users.noreply.github.com>
2026-02-12 15:35:13 -0800

    Updating admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,29 +1,48 @@
 from django.contrib import admin
-from .models import Office, Party, Candidate, Employer, Contributor, Contribution, FECContribution, Committee
+from .models import Party, Candidate, Employer, Contributor, Contribution, FECContribution, Committee
 
-@admin.register(FECContribution)
-class FECContributionAdmin(admin.ModelAdmin):
-    list_display = ('NAME', 'TRANSACTION_DT', 'TRANSACTION_AMT', 'ENTITY_TP', 'CMTE_ID')
-    list_filter = ('ENTITY_TP', 'STATE', 'TRANSACTION_DT')
-    search_fields = ('NAME', 'CMTE_ID', 'TRAN_ID')
-    date_hierarchy = 'TRANSACTION_DT'
-
-@admin.register(Committee)
-class CommitteeAdmin(admin.ModelAdmin):
-    list_display = ('CMTE_NM', 'CMTE_ID', 'CMTE_TP', 'CMTE_ST')
-    list_filter = ('CMTE_TP', 'CMTE_ST', 'CMTE_DSGN')
-    search_fields = ('CMTE_NM', 'CMTE_ID', 'TRES_NM')
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('id', 'name')
 
 @admin.register(Candidate)
 class CandidateAdmin(admin.ModelAdmin):
-    # Updated to use the new field names from the FEC dictionary
-    list_display = ('CAND_NAME', 'CAND_ID', 'CAND_OFFICE', 'CAND_PTY_AFFILIATION', 'CAND_ELECTION_YR')
+    list_display = ('CAND_NAME', 'CAND_ID', 'CAND_PTY_AFFILIATION', 'CAND_ELECTION_YR', 'CAND_OFFICE')
     list_filter = ('CAND_PTY_AFFILIATION', 'CAND_OFFICE', 'CAND_ELECTION_YR')
     search_fields = ('CAND_NAME', 'CAND_ID')
+    autocomplete_fields = ['CAND_PTY_AFFILIATION'] # Improves performance for dropdowns
 
-# Registering the simpler models with default views
-admin.site.register(Office)
-admin.site.register(Party)
-admin.site.register(Employer)
-admin.site.register(Contributor)
-admin.site.register(Contribution)
+@admin.register(Committee)
+class CommitteeAdmin(admin.ModelAdmin):
+    list_display = ('CMTE_NM', 'CMTE_ID', 'CMTE_TP', 'CAND_ID')
+    list_filter = ('CMTE_TP', 'CMTE_ST', 'CMTE_DSGN')
+    search_fields = ('CMTE_NM', 'CMTE_ID')
+    raw_id_fields = ('CAND_ID',) # Better for large candidate tables
+
+@admin.register(Employer)
+class EmployerAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'zip_code', 'employer')
+    search_fields = ('full_name', 'zip_code')
+    list_select_related = ('employer',) # Performance optimization for 8GB RAM
+
+@admin.register(Contribution)
+class ContributionAdmin(admin.ModelAdmin):
+    list_display = ('contributor', 'committee', 'amount', 'receipt_date')
+    list_filter = ('receipt_date',)
+    date_hierarchy = 'receipt_date'
+    # Prevents N+1 queries in the admin list view
+    list_select_related = ('contributor', 'committee')
+    raw_id_fields = ('contributor', 'committee')
+
+@admin.register(FECContribution)
+class FECContributionAdmin(admin.ModelAdmin):
+    list_display = ('NAME', 'TRANSACTION_DT', 'TRANSACTION_AMT', 'CMTE_ID')
+    list_filter = ('STATE', 'TRANSACTION_DT')
+    search_fields = ('NAME', 'CMTE_ID')
+    show_full_result_count = False # Faster loading for millions of rows
