fd57cd08b0e992978f14c1e55904113d3c9dcaca
Ben Stearman <bms08140@vermontstate.edu>
2026-02-14 17:30:35 -0500

    Updating versions and .gitignore

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -51,7 +51,6 @@ dependencies {
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     implementation(libs.androidx.navigation.compose)
-    implementation("androidx.navigation:navigation-compose:2.8.5")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
