43c156a8c19f071ce33b04103cff6287fcdfa148
Ben Stearman <bms08140@vermontstate.edu>
2026-02-20 00:36:18 -0500

    Creating stub for CandidateDetailScreen

--- frontend/app/src/main/java/app/contribs/data/model/CandidateModel.kt
+++ frontend/app/src/main/java/app/contribs/data/model/CandidateModel.kt
@@ -1,9 +1,1 @@
 package app.contribs.data.model
-
-data class Candidate(
-    val id: String,
-    val name: String,
-    val party: String?,
-    val state: String?,
-    val office: String?
-)
--- frontend/app/src/main/java/app/contribs/ui/candidates/CandidateViewModel.kt
+++ frontend/app/src/main/java/app/contribs/ui/candidates/CandidateViewModel.kt
@@ -8,8 +8,8 @@ import app.contribs.data.model.Candidate
 class CandidateViewModel : ViewModel() {
     private val _candidates = MutableStateFlow<List<Candidate>>(
         listOf(
-            Candidate("P00000001", "Bernie Sanders", "DEM", "VT", "P"),
-            Candidate("P00000002", "Donald Trump", "REP", "FL", "P")
+            Candidate("P00000001", "Bernie Sanders", "DEM", 2024, "VT", "P"),
+            Candidate("P00000002", "Donald Trump", "REP", 2024, "FL", "P")
         )
     )
     val candidates: StateFlow<List<Candidate>> = _candidates
