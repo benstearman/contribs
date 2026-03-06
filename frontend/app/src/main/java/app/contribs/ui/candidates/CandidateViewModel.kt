2fb214099b11e78a74c9f434ef16ee522abcb462
dmkvt <dmkvt@protonmail.com>
2026-03-06 10:39:58 -0500

    update CandidateViewModel

--- frontend/app/src/main/java/app/contribs/ui/candidates/CandidateViewModel.kt
+++ frontend/app/src/main/java/app/contribs/ui/candidates/CandidateViewModel.kt
@@ -1,18 +1,33 @@
 package app.contribs.ui.candidates
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.contribs.data.api.RetrofitClient
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import app.contribs.data.model.Candidate
+import kotlinx.coroutines.launch
 
 class CandidateViewModel : ViewModel() {
-    private val _candidates = MutableStateFlow<List<Candidate>>(
-        listOf(
-            Candidate("P00000001", "Bernie Sanders", "IND", 2024, "VT", "P"),
-            Candidate("P00000002", "Donald Trump", "REP", 2024, "FL", "P")
-        )
-    )
+    private val _candidates = MutableStateFlow<List<Candidate>>(emptyList())
     val candidates: StateFlow<List<Candidate>> = _candidates
+
+    init {
+        fetchCandidates()
+    }
+
+    private fun fetchCandidates() {
+        viewModelScope.launch {
+            try {
+                val response = RetrofitClient.instance.getCandidates()
+                _candidates.value = response
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
+
+    }
 
     // Add this function to find a specific candidate for the detail screen
     fun getCandidateById(id: String): Candidate? {
