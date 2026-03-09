ccbeea83e0ea5be82c009c82a708aff482522f39
Ben Stearman <bms08140@vermontstate.edu>
2026-03-09 12:29:54 -0400

    Using calculated total contributions field for candidate detail

--- frontend/app/src/main/java/app/contribs/data/model/CandidateModel.kt
+++ frontend/app/src/main/java/app/contribs/data/model/CandidateModel.kt
@@ -5,8 +5,9 @@ import com.google.gson.annotations.SerializedName
 data class Candidate(
     @SerializedName("CAND_ID") val id: String,
     @SerializedName("CAND_NAME") val name: String,
-    @SerializedName("CAND_PTY_AFFILIATION") val party: String?,
+    @SerializedName("party_name") val party: String?,
     @SerializedName("CAND_ELECTION_YR") val electionYear: Int?,
     @SerializedName("CAND_OFFICE_ST") val state: String?,
-    @SerializedName("office_display") val office: String?
+    @SerializedName("office_display") val office: String?,
+    @SerializedName("total_contributions") val totalContributions: Double?
 )
