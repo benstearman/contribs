b5e86ab672294a51e34c6180e01675fbe79a946b
mastigos <88701209+mastigos@users.noreply.github.com>
2026-03-20 10:22:29 -0400

    Forgot to add filters import

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.db.models import Sum
