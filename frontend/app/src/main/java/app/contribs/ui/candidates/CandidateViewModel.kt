7ce6a16a004552d8d6f2f83d222a5079e093d903
Ben Stearman <bms08140@vermontstate.edu>
2026-04-25 11:36:17 -0400

    fix: resolve build error by adding missing import

--- frontend/app/src/main/java/app/contribs/ui/candidates/CandidateViewModel.kt
+++ frontend/app/src/main/java/app/contribs/ui/candidates/CandidateViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class CandidateViewModel : ViewModel() {
